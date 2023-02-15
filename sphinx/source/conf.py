1868f797dbc3e3a7b15db587b2a2d365ebf9fe6d
Eirik Haustveit <eirik@novitech.no>
2023-02-15 22:35:53 +0100

    Started a new Sphinx project for documentation of the examples

---
+++ sphinx/source/conf.py
@@ -0,0 +1,29 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'STM32 programming by examples'
+copyright = '2023, Eirik Haustveit'
+author = 'Eirik Haustveit'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+#html_theme = 'alabaster'
+html_theme = 'sphinx_book_theme'
+html_static_path = ['_static']
